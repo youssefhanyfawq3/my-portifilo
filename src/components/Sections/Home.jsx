d4071d4e1a8aec509eea36dfa3fee6a305db1677
youssefhn632 <youssefhn632@gmail.com>
2025-04-02 03:02:49 +0000

    small edit in home page

--- src/components/Sections/Home.jsx
+++ src/components/Sections/Home.jsx
@@ -9,8 +9,7 @@ export const Home  = () => {
             </h1>
 
             <p className="text-gray-400 text-lg mb-8 mt-12 max-w-lg mx-auto">
-                i am Full stack developer who loves crafting clean, scalable web applications
-            </p>
+            Full Stack Developer crafting scalable, innovative web applications with clean, maintainable code.            </p>
             <div className="flex justify-center space-x-4">
                 <a href="#projects" className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:translate-y-0.5 hover:shadow-[0_0_15px_rgba(59,130,246,0.4)]" >View Projects</a>
                 <a href="#contact" className=" border border-blue-500/50 py-3 px-6 rounded font-medium transition-all duration-200  hover:-translate-0.5  hover:shadow-[0_0_15px_rgba(59,130,246,0.2)] hover:-bg-blue-500  " >Contact</a>
